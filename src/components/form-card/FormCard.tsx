738167f746caca50294ba7b3b12d2dd5200b38ff
TigranBabujyan <tigran.babujyan@gmail.com>
2024-05-31 21:36:18 +0400

    refactor, validation partly, bugfixes

--- src/components/form-card/FormCard.tsx
+++ src/components/form-card/FormCard.tsx
@@ -1,7 +1,6 @@
 import React, {useState} from 'react'
 import './index.css'
 import {NewDataTypes} from "../../types/adminData";
-import {Link} from "react-router-dom";
 import {editData} from '../../lib/controller'
 import {useNavigate} from "react-router-dom";
 import { Button, Input, Radio, Space} from "antd";
@@ -24,43 +23,42 @@ function  SOSFormCard({apiData}: IProps) {
     const [selectedSocial, setSelectedSocial] = useState('')
     const [selectedNumber, setSelectedNumber] = useState('')
     const [selectedEmail, setSelectedEmail] = useState('')
+    const [requirement, setRequirement] = useState(false)
 
     const navigate = useNavigate()
 
 
     const handleRadioChangeEvent = (e: any) => {
          setSelectedEvent(e.target.value)
-        console.log (selectedEvent, 'selected event')
     }
     const handleRadioChangeSocial = (e: any) => {
         setSelectedSocial(e.target.value)
-        console.log (selectedSocial, 'selected Social')
     }
     const handleRadioChangeName = (e: any) => {
         setSelectedName(e.target.value)
-        console.log (selectedName, 'selected Name')
     }
     const handleRadioChangeSurname = (e: any) => {
         setSelectedSurname(e.target.value)
-        console.log (selectedSurname, 'selected Surname')
     }
     const handleRadioChangeNumber = (e: any) => {
         setSelectedNumber(e.target.value)
-        console.log (selectedNumber, 'selected Number')
     }
 
     const handleRadioChangeEmail = (e: any) => {
         setSelectedEmail(e.target.value)
-        console.log (selectedEmail, 'selected Email')
     }
 
     const handleRadioChangeTransfer = (e: any) => {
         setTransferAnswer(e.target.value)
-        console.log (transferAnswer, 'selected Email')
     }
 
     const handleSubmit = () => {
-        navigate(`${selectedEvent === apiData.event1 ? '/requirements' : '/agreement'}`)
+        if (selectedSocial === '' || selectedEmail === '' || selectedEvent === '' || selectedName === '' || selectedSurname === '' || selectedNumber === '') {
+            alert('Please enter Required fields!')
+            setRequirement(true)
+        } else {
+            navigate(`${selectedEvent === apiData.event1 ? '/requirements' : '/agreement'}`)
+        }
     }
     const changeEventDetails = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -71,9 +69,7 @@ function  SOSFormCard({apiData}: IProps) {
 
         try {
             await editData('admin_input', data)
-            console.log('successfully changed')
         } catch (error) {
-            console.error("Error updating document: ", error);
         }
         handleSubmit()
     };
@@ -88,9 +84,9 @@ function  SOSFormCard({apiData}: IProps) {
                         <div className='title'>Event Details</div>
                         <Radio.Group onChange={handleRadioChangeEvent}>
                             <Space className='event_wrap' direction="vertical">
-                                <Radio className='text_correction' value={`${apiData.event1}`}>{apiData.event1}</Radio>
-                                <Radio className='text_correction' value={`${apiData.event2}`}>{apiData.event2}</Radio>
-                                <Radio className='text_correction' value={`${apiData.event3}`}>{apiData.event3}</Radio>
+                                <Radio className = {`${requirement &&  selectedEvent === '' ? 'required' : 'text_correction'}`} value={`${apiData.event1}`}>{apiData.event1}</Radio>
+                                <Radio className = {`${requirement &&  selectedEvent === '' ? 'required' : 'text_correction'}`} value={`${apiData.event2}`}>{apiData.event2}</Radio>
+                                <Radio className = {`${requirement &&  selectedEvent === '' ? 'required' : 'text_correction'}`} value={`${apiData.event3}`}>{apiData.event3}</Radio>
                             </Space>
                         </Radio.Group>
                     </form>
@@ -100,10 +96,10 @@ function  SOSFormCard({apiData}: IProps) {
                     <form onChange={handleRadioChangeTransfer}>
                         <Radio.Group>
                             <Space direction="vertical">
-                                <Radio onChange={() => {
+                                <Radio className = {`${requirement && transferAnswer === '' ? 'required' : 'text_correction'}`} onChange={() => {
                                     setTransferAnswer("YES")
                                 }} value='YES'>YES</Radio>
-                                <Radio onChange={() => {
+                                <Radio className = {`${requirement && transferAnswer === '' ? 'required' : 'text_correction'}`} onChange={() => {
                                     setTransferAnswer("NO")
                                 }} value='NO'>NO</Radio>
                             </Space>
@@ -114,24 +110,52 @@ function  SOSFormCard({apiData}: IProps) {
                     <div className='title'>{apiData.social_header}</div>
                     <form>
                         <div className='event_wrap'>
-                            <Radio.Group onChange={handleRadioChangeEvent}>
+                            <Radio.Group onChange={handleRadioChangeSocial}>
                                 <Space className='event_wrap' direction="vertical">
-                                    <Radio value={`${apiData.social1}`}>{apiData.social1}</Radio>
-                                    <Radio value={`${apiData.social2}`}>{apiData.social2}</Radio>
-                                    <Radio value={`${apiData.social3}`}>{apiData.social3}</Radio>
-                                    <Radio value={`${apiData.social4}`}>{apiData.social4}</Radio>
-                                    <Radio value={`${apiData.social5}`}>{apiData.social5}</Radio>
-                                    <Radio value={`${apiData.social6}`}>
-                                        {selectedEvent === apiData.social6 ? (
+                                    <Radio
+                                        className = {`${requirement && selectedSocial === '' ? 'required' : 'text_correction'}`}
+                                        value={`${apiData.social1}`}
+                                    >
+                                        {apiData.social1}
+                                    </Radio>
+                                    <Radio
+                                        className = {`${requirement && selectedSocial === '' ? 'required' : 'text_correction'}`}
+                                        value={`${apiData.social2}`}
+                                    >
+                                        {apiData.social2}
+                                    </Radio>
+                                    <Radio
+                                        className = {`${requirement && selectedSocial === '' ? 'required' : 'text_correction'}`}
+                                        value={`${apiData.social3}`}
+                                    >
+                                        {apiData.social3}
+                                    </Radio>
+                                    <Radio
+                                        className = {`${requirement && selectedSocial === '' ? 'required' : 'text_correction'}`}
+                                        value={`${apiData.social4}`}
+                                    >
+                                        {apiData.social4}
+                                    </Radio>
+                                    <Radio
+                                        className = {`${requirement && selectedSocial === '' ? 'required' : 'text_correction'}`}
+                                        value={`${apiData.social5}`}>
+                                        {selectedSocial === apiData.social5 ? (
                                             <Input
-                                                style={{
-                                                    width: 100,
-                                                    marginLeft: 10,
-                                                    display: 'flex',
-                                                    flexDirection: 'column'
-                                                }}
+                                                className = {`${requirement && selectedSocial === ''? 'required' : 'input_text_radio'}`}
+                                                placeholder = 'Name'
                                             />
-                                        ) : 'More...'}
+
+                                        ) : `${apiData.social5}`}
+                                    </Radio>
+                                    <Radio
+                                        className = {`${requirement && selectedSocial === '' ? 'required' : 'text_correction'}`}
+                                        value={`${apiData.social6}`}>
+                                        {selectedSocial === apiData.social6 ? (
+                                            <Input
+                                                className = {`${requirement && selectedSocial === ''? 'required' : 'input_text_radio'}`}
+                                                placeholder = 'Name'
+                                            />
+                                        ) : 'Other...'}
                                     </Radio>
                                 </Space>
                             </Radio.Group>
@@ -140,30 +164,48 @@ function  SOSFormCard({apiData}: IProps) {
                     <form>
                         <div className="input">
                             <div className='title'>Name/Имя/Անուն</div>
-                            <Input type="text" className='input_text' id='name' name="fav_icon"
-                                   placeholder='Enter Your Name' required
-                                   onChange={handleRadioChangeName}/>
+                            <Input
+                                type="text"
+                                className={`${requirement && selectedName === '' ? 'required': 'input_text'}`}
+                                id='name'
+                                name="fav_icon"
+                                placeholder='Enter Your Name'
+                                required
+                                onChange={handleRadioChangeName}/>
                             <label htmlFor='surname'>{apiData.name}</label>
                         </div>
                         <div className="input">
                             <div className='title'>Surname/Фамилия/Ազգանուն</div>
-                            <Input type="text" className='input_text' id='surname' name="fav_icon"
-                                   placeholder='Enter Your Surname' required
-                                   onChange={handleRadioChangeSurname}/>
+                            <Input
+                                type="text"
+                                className= {`${requirement && selectedSurname === ''? 'required' : 'input_text'}`}
+                                id='surname'
+                                name="fav_icon"
+                                placeholder='Enter Your Surname'
+                                required
+                                onChange={handleRadioChangeSurname}/>
                             <label htmlFor='surname'>{apiData.surname}</label>
                         </div>
                         <div className='number'>
                             <form>
                                 <div className='title'>{apiData.number_title}</div>
-                                <Input type='text' className='input_text' id='number' placeholder='Enter Your Number!'
-                                       onChange={handleRadioChangeNumber}/>
+                                <Input
+                                    type='text'
+                                    className={`${requirement && selectedNumber === '' ? 'required': 'input_text'}`}
+                                    id='number'
+                                    placeholder='Enter Your Number!'
+                                    onChange={handleRadioChangeNumber}/>
                             </form>
                         </div>
                         <div className='email'>
                             <form>
                                 <div className='title'>{apiData.email_title}</div>
-                                <Input type='text' className='input_text' id='number' placeholder='Enter Your Number!'
-                                       onChange={handleRadioChangeEmail}/>
+                                <Input
+                                    type='email'
+                                    className={`${requirement && selectedEmail === '' ? 'required' :'input_text'}`}
+                                    id='email'
+                                    placeholder='Enter Your Email!'
+                                    onChange={handleRadioChangeEmail}/>
                             </form>
                         </div>
                     </form>
